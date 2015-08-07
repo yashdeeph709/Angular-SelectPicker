9984604e0e8caa01953577694d4e20bd46be2c02
yashdeep hinge <yashdeeph709@gmail.com>
2015-08-07 17:57:48 +0530

    Initial started

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
   var app = angular.module('SelectV2', []);
-  app.directive('selectv2', function () {
+  app.controller('basiccontroller', function ($scope) {
+      $scope.options = ['abc', 'def', 'ghi'];
+  });
+  app.directive('selectv2', function ($timeout) {
       return {
-          restrict: "E",
-          templateUrl: 'partials/select.html'
+          restrict: 'E',
+          templateUrl: 'partials/select.html',
+          scope: {
+              options: "=options"
+          },
+          require: '?ngModel',
+          replace: true,
+          link: function (scope, elem, attrs, ngModelCtrl) {
+              scope.open = function () {
+                      console.log("open called");
+                      if (scope.class === "open") {
+                          scope.class = '';
+                          return;
+                      }
+                      scope.class = "open";
+                  }
+                  //for ngmodel
+              if (ngModelCtrl) {
+                  $timeout(function () {
+                      ngModelCtrl.$render = function () {
+                          editor.setValue(ngModelCtrl.$viewValue);
+                      }
+                  })
+              }
+
+          }
       }
   });

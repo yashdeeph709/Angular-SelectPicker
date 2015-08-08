1341005c8abb32062332737258913e4d05851820
yashdeep hinge <yashdeeph709@gmail.com>
2015-08-08 09:00:30 +0530

    filter done!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 var app = angular.module('app', ['SelectV2']);
-app.controller('basiccontroller', function ($scope) {
-    $scope.options = ['abc', 'def', 'ghi'];
+app.controller('basiccontroller', function($scope) {
+   $scope.options = [{
+      option: 'veg',
+      class: 'dropdown-header'
+   }, {
+      option: 'vege'
+   }, {
+      option: 'veget',
+      class: 'dropdown-header'
+   }, {
+      option: 'vegeta'
+   }, {
+      option: 'vegeta'
+   }];
+   $scope.catagories = ['veg', 'nonveg'];
 });
--- js/selectv2.js
+++ js/selectv2.js
@@ -5,13 +5,12 @@
          restrict: 'E',
          templateUrl: 'partials/select.html',
          scope: {
-            options: "=options"
+            options: "=options",
+            catagories: "=catagories"
          },
-         require: '?ngModel',
          replace: true,
-         link: function(scope, elem, attrs, ngModelCtrl) {
+         link: function(scope, elem, attrs) {
             scope.selected = scope.options[0];
-            console.log(elem);
             scope.open = function() {
                if (scope.class === "open") {
                   scope.class = '';

69de3fcf29e41333d50474163bf0b25902b664e5
yashdeep hinge <yashdeeph709@gmail.com>
2015-08-08 09:11:05 +0530

    fooles commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,5 @@
 var app = angular.module('app', ['SelectV2']);
 app.controller('basiccontroller', function($scope) {
-   $scope.options = [{
-      option: 'veg',
-      class: 'dropdown-header'
-   }, {
-      option: 'vege'
-   }, {
-      option: 'veget',
-      class: 'dropdown-header'
-   }, {
-      option: 'vegeta'
-   }, {
-      option: 'vegeta'
-   }];
-   $scope.catagories = ['veg', 'nonveg'];
+   $scope.data.options = ['veg', 'vege', 'veget', 'vegeta', 'vegeta'];
+   $scope.data.catagories = ['veg', 'nonveg'];
 });

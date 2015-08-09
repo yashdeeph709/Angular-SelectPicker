46c95cf6637f5f8cee19e3f21c241206f68bd5e5
yashdeep hinge <yashdeeph709@gmail.com>
2015-08-09 22:36:35 +0530

    structuring done!

--- js/app.js
+++ js/app.js
@@ -3,7 +3,4 @@ app.controller('basiccontroller', function($scope, $timeout) {
    $scope.data = {};
    $scope.data.options = ['veg', 'vege', 'veget', 'vegeta', 'nonveg', 'vegeqt', 'vegeqta', 'vegeqtsa'];
    $scope.data.catagories = ['veg', 'nonveg'];
-   $timeout(function() {
-      $scope.value = $scope.data.options[3];
-   }, 3000);
 });
--- js/selectv2.js
+++ js/selectv2.js
@@ -15,8 +15,8 @@
       strVar += "";
       $templateCache.put('selectv2.html', strVar);
    });
-
-   app.directive('selectv2', function($timeout, $document) {
+   app.directive('selectv2', function($timeout) {
+      //Directive Defination Object
       return {
          restrict: 'E',
          templateUrl: "selectv2.html",
@@ -28,99 +28,113 @@
          replace: true,
          link: linkFn
       }
-
+      //Link
       function linkFn(scope, element, attrs, ngModelCtrl) {
-         scope.selected = attrs.default;
-         var x = attrs.maxopt - 1;
+         //template generator
+         dropdown(scope.options, scope.catagories, attrs.livesearch, element, select);
+         //waiting for model mutation and regenration
+         scope.$watch(scope.options, function(oldvalue, newvalue) {
+            console.log("line 35:change seen by directive in model")
+            if (oldvalue !== newvalue) {
+               dropdown(scope.options, scope.catagories, attrs.livesearch, element, select);
+            }
+         });
+         //dropdown opening logic
+         var button = angular.element(element.children()[0]);
+         button.bind("click", function(e) {
+            angular.element(element.children()[1]).toggleClass("bounce");
+            element.toggleClass("open");
+         });
+
          ngModelCtrl.$render = function() {
             console.log("line 36: render called by angular with " + ngModelCtrl.$viewValue);
             $timeout(function() {
                scope.selected = ngModelCtrl.$viewValue;
             });
          }
-         open = function(e) {
-            element.toggleClass("open");
-         }
-         scope.select = function(id) {
+
+         function select(id) {
             console.log("line 47:select called!");
-            $timeout(function() {
-               if (attrs.multi && scope.selected !== attrs.default) {
-                  console.log("line 50:multi select on and not first click");
-                  if (x > 0) {
-                     console.log("line 52:Max is far");
-                     scope.selected = scope.selected + "," + scope.options[id];
-                     ngModelCtrl.$setViewValue(scope.selected);
-                     ngModelCtrl.$render();
-                     x--;
-                     return true;
-                  } else {
-                     //callback should be user defined
-                     console.log("line 60:Max is happend");
-                     alert("max exceeded");
-                     return false;
-                  }
-               } else {
-                  console.log("line 65:single select is on");
-                  scope.selected = scope.options[id];
-                  ngModelCtrl.$setViewValue(scope.selected);
-                  ngModelCtrl.$render();
-                  return true;
-               }
-            });
-
-            element.removeClass("open");
-         }
-         var button = angular.element(element.children()[0]);
-         if (!attrs.livesearch) {
-            var input = angular.element(element.children()[1]);
-            input.children()[0].remove();
-         }
-         var ul = angular.element("<ul>");
-         ul.addClass("dropdown-menu inner");
-         list(ul, scope, attrs);
-         var dropdown = angular.element(element.children()[1]);
-         dropdown.append(ul);
-         button.bind("click", open);
-      }
-
-      function list(ul, scope, attr) {
-         for (var i = 0; i < scope.options.length; i++) {
-            var li = angular.element("<li>");
-            if (scope.catagories !== undefined) {
-               if (scope.catagories.indexOf(scope.options[i]) === -1) {
-                  var a = angular.element("<a class='opt'>");
-                  a.append(angular.element("<span class='text'>" + scope.options[i] + ""));
-                  a.append(angular.element("<span class='glyphicon glyphicon-ok check-mark'>"));
-                  li.append(a)
-                  li.attr("index", i);
-                  li.bind("click", function(e) {
-                     var index = angular.element(this).attr("index");
-                     if (scope.select(index) && attr.multi) {
-                        console.log("tooggling on");
-                        angular.element(angular.element(li.children()[0]).children()[1]).toggleClass("check-mark");
-                     }
-                  });
-               } else {
-                  i !== 0 ? ul.append(angular.element("<li class='divider'>")) : null;
-                  li.addClass("dropdown-header");
-                  li.append(angular.element("<span class='text'>" + scope.options[i] + ""))
-               }
+            if (attrs.multi && scope.selected !== ngModelCtrl.$viewValue) {
+               console.log("line 50:multi select on and not first click");
+               scope.selected = scope.selected + "," + scope.options[id];
             } else {
-               var a = angular.element("<a class='opt'>");
-               a.append(angular.element("<span class='text'>" + scope.options[i] + ""));
-               a.append(angular.element("<span class='glyphicon glyphicon-ok check-mark'>"));
-               li.append(a);
-               li.attr("index", i);
-               li.bind("click", function(e) {
-                  var index = angular.element(this).attr("index");
-                  var li = angular.element(this).addClass("selected");
-                  angular.element(this).addClass("selected");
-                  if (scope.select(index) && attr.multi) {
-                     angular.element(angular.element(li.children()[0]).children()[1]).toggleClass("check-mark");
-                  }
-               });
+               console.log("line 65:single select is on");
+               scope.selected = scope.options[id];
             }
-            ul.append(li);
+            ngModelCtrl.$setViewValue(scope.selected);
+            ngModelCtrl.$render();
+            element.removeClass("open");
+            return true;
          }
-      }
+      } //link function over
    });
+   /* Templater class that hold the whole functions */
+   function Templater() {}
+   Templater.prototype.dropdown = dropdown;
+   Templater.prototype.genList = genList;
+   Templater.prototype.li = li;
+   Templater.prototype.insideLi = insideLi;
+   Templater.prototype.livesearch = livesearch;
+   Templater.prototype.bind = bind;
+
+   //function that generates dropdown
+   function dropdown(options, catagories, search, element, select) {
+      console.log("line 89:drop down render called")
+      var dropdown = angular.element(element.children()[1]);
+      var list = genList(options, catagories);
+      livesearch(search, element);
+      bind(list, select);
+      dropdown.append(list);
+   }
+
+   //function that generates unordered list
+   function genList(options, catagories) {
+      var ul = angular.element("<ul>");
+      ul.addClass("dropdown-menu inner");
+      for (var i = 0; i < options.length; i++) {
+         ul.append(li(options, catagories, i));
+      }
+      return ul;
+   }
+
+   //FUNCTION DEDICATED FOR GENERATING LIST ITEMS AND BINDING CLICK EVENT TO THEM;
+   function li(options, catagories, i) {
+      var li = angular.element("<li>");
+      if (angular.isDefined(catagories) && catagories.indexOf(options[i]) !== -1) {
+         li.addClass("dropdown-header");
+         li.append(angular.element("<span class='text'>" + options[i] + ""))
+      } else {
+         li.append(insideLi(options, i));
+         li.attr("index", i);
+      }
+      return li;
+   }
+
+   function insideLi(options, i) {
+      var a = angular.element("<a class='opt'>");
+      a.append(angular.element("<span class='text'>" + options[i] + ""));
+      a.append(angular.element("<span class='glyphicon glyphicon-ok check-mark'>"));
+      return a;
+   }
+
+   function livesearch(search, element) {
+      if (!search) {
+         var input = angular.element(element.children()[1]);
+         input.children()[0].remove();
+      }
+   }
+
+   function bind(ul, select) {
+      for (var i = 0; i < ul.children().length; i++) {
+         var li = angular.element(ul.children()[i]);
+         li.bind("click", function(e) {
+            var index = angular.element(this).attr("index");
+            if (typeof select !== "function") {
+               console.log("not a callback");
+               return;
+            }
+            select(index);
+         });
+      }
+   }
